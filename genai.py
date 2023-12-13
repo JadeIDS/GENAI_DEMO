8e739892a5db9fbbfe96e0a4185485345b09d891
JadeIDS <153002431+JadeIDS@users.noreply.github.com>
2023-12-13 12:00:02 +0530

    Update genai.py

--- genai.py
+++ genai.py
@@ -32,7 +32,7 @@ password=st.secrets["streamlit_password"]
 
 #  establish OpenAI connection
 llm = ChatOpenAI(
-    model_name="gpt-4",
+    model_name="gpt-3.5-turbo",
     temperature=0,
     max_tokens=2000,
     openai_api_key= st.secrets["openai_key"])
